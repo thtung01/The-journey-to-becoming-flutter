b5c0c3c31be0bf3776f565fc5aa3a0cef9174873
tung <tungcoder007@gmail.com>
2026-04-19 21:45:32 +0700

    day 10 to becoming flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,8 @@
 import "package:flutter/material.dart";
+import 'package:my_app/model/day9.dart';
 void main() {
   runApp(MaterialApp(
-    home: SafeArea(
-      child: Scaffold(
-        body: MyWidget()
-      )
-      ),
-      debugShowCheckedModeBanner: false,
-      ));
-}
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.green,
-        width: 200,
-        height: 200,
-        alignment: Alignment.center,
-        child: const Text("Tung", style: TextStyle(color: Colors.white),)
-      ),
-    );
-  }
+    home: Day9Screen(),    
+    debugShowCheckedModeBanner: false,
+  ));
 }
--- lib/model/day9.dart
+++ lib/model/day9.dart
@@ -1,1 +1,50 @@
-// ngay mai se lam bai tap trang ca nhan cua ngay hom nay 
+import 'package:flutter/material.dart';
+
+class Day9Screen extends StatefulWidget{
+    @override
+    _Day9ScreenState createState() => _Day9ScreenState() ;
+}
+class _Day9ScreenState extends State<Day9Screen> {
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: Colors.white,
+            body: SafeArea(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                        const SizedBox(height:20),
+                        Center(
+                          child: Container(
+                              width: 120,
+                              height: 120,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.green,
+                                  border: Border.all(width: 2, color: Colors.red),
+                              ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Center(
+                          child: Container(
+                              child: const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                      Text("Name: Nguyen Thanh Tung", style: TextStyle(color: Colors.pink, fontSize: 19),),
+                                      const SizedBox(height: 20),
+                                      Text("Age: 20", style: TextStyle(color:Colors.pink, fontSize: 19),),
+                                      const SizedBox(height: 20),
+                                      Text("Email: nguyenthanhtungtttb@gmail.com", style: TextStyle(color:Colors.pink, fontSize: 19),),
+                                      const SizedBox(height: 20),
+                                      Text("Address: From Thai Binh", style: TextStyle(fontSize: 19, color:Colors.pink),),
+                                  ],
+                              ),
+                          ),
+                        )
+                    ],
+                ),
+                ),   
+        );
+    }
+}
