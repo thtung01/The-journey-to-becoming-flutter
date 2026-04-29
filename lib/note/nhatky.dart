388f9d0398458ec250e7fa44c92f8ebd102aef81
tung <tungcoder007@gmail.com>
2026-04-29 22:40:30 +0700

    ...

---
+++ lib/note/nhatky.dart
@@ -0,0 +1,12 @@
+/*
+Hôm nay là ngày 29/4/2026, đã là ngày thứ 20 tôi bắt đầu theo đuổi con đường này,
+những ngày đầu thì mình cảm thấy rất hứng thú nhưng không kéo dài được lâu,
+chỉ sau vài hôm, tôi đã trì hoãn hơn 1 tuần nay, mình cảm giác 
+mình không đủ can đảm để bước tiếp, mình mông lung không biết tương lai đi về đâu,
+cũng có chút ghen tị, ngưỡng mộ bạn mình đang có nhiều cơ hội tốt để phát triển,
+đôi khi cũng là vì gia đình.
+Trong lòng có rất nhiều điều muốn nói ra nhưng thôi, sắp sang tuổi mới rồi,
+mình hứa với bản thân là sẽ không còn chần chừ nữa, sẽ tự mình cố gắng phấn đấu,
+tự tìm hiểu con đường và kiên trì với nó, không lung lay, không thấy thất vọng về bản thân.
+Mục tiêu cho tuổi mới:735+ toeic, có 1 dự án app trong tay, có chỗ làm bán thời gian, thể thao lành mạnh.
+*/
